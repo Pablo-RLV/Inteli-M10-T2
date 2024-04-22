1276ba966cc23741eddede87f7bb9810175bd811
Pablo Ruan <pablo.viana@sou.inteli.edu.br>
2024-04-22 18:24:19 -0300

    refactor(Ponderada1): :recycle: separate CRUD in files

--- ponderada1/src/routes/notes.py
+++ ponderada1/src/routes/notes.py
@@ -2,45 +2,40 @@ from database.database import db
 from database.models import Notes
 from flask import jsonify, request, Blueprint
 from flask_jwt_extended import jwt_required
+from common.read import get_all_data, get_data_by_id
+from common.delete import delete_data_by_id
+from common.create import create_data
+from common.update import update_data
 
 notes_routes = Blueprint('note_routes', __name__)
 
 @notes_routes.route('/notes', methods=["GET"])
 @jwt_required()
 def get_notes():
-    notes = Notes.query.all()
-    return_notes = []
-    for note in notes:
-        return_notes.append(note.serialize())
-    return jsonify(return_notes)
+    result = get_all_data(Notes)
+    return jsonify(result)
 
 @notes_routes.route('/notes' , methods = ['POST'])
 @jwt_required()
 def create_note():
     text = request.form.get("text", None)
-    note = Notes(text=text)
-    db.session.add(note)
-    db.session.commit()
-    return jsonify(note.serialize())
+    result = create_data(Notes, db, text=text)
+    return result
 
 @notes_routes.route("/notes/<int:id>", methods=["GET"])
 def get_note(id):
-    note = Notes.query.get(id)
-    return jsonify(note.serialize())
+    result = get_data_by_id(Notes, id)
+    return jsonify(result)
     
 @notes_routes.route('/notes/<int:id>',methods = ['PUT'])
 @jwt_required()
 def update_note(id):
     new_text = request.form.get("text", None)
-    note = Notes.query.get(id)
-    note.text = new_text
-    db.session.commit()
-    return jsonify(note.serialize())
+    result = update_data(Notes, db, id, text=new_text)
+    return jsonify(result)
 
 @notes_routes.route('/notes/<int:id>', methods=['DELETE'])
 @jwt_required()
 def delete_note(id):
-    note = Notes.query.get(id)
-    db.session.delete(note)
-    db.session.commit()
-    return jsonify(note.serialize())
+    result = delete_data_by_id(Notes, id, db)
+    return jsonify(result)
--- ponderada1/src/routes/users.py
+++ ponderada1/src/routes/users.py
@@ -1,44 +1,39 @@
 from database.database import db
 from database.models import User
 from flask import jsonify, request, Blueprint
+from common.read import get_all_data, get_data_by_id
+from common.delete import delete_data_by_id
+from common.create import create_data
+from common.update import update_data
 
 users_routes = Blueprint('users_routes', __name__)
 
 @users_routes.route("/users", methods=["GET"])
 def get_users():
-    users = User.query.all()
-    return_users = []
-    for user in users:
-        return_users.append(user.serialize())
-    return jsonify(return_users)
+    result = get_all_data(User)
+    return jsonify(result)
 
 @users_routes.route("/users", methods=["POST"])
 def create_user():
     username = request.form.get("username", None)
     password = request.form.get("password", None)
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.serialize())
+    result = create_data(User, db, username=username, password=password)
+    return result
+
 
 @users_routes.route("/users/<int:id>", methods=["GET"])
 def get_user(id):
-    user = User.query.get(id)
-    return jsonify(user.serialize())
+    result = get_data_by_id(User, id)
+    return jsonify(result)
 
 @users_routes.route("/users/<int:id>", methods=["PUT"])
 def update_user(id):
-    username = request.form.get("username", None)
-    password = request.form.get("password", None)
-    user = User.query.get(id)
-    user.username = username
-    user.password = password
-    db.session.commit()
-    return jsonify(user.serialize())
+    new_username = request.form.get("username", None)
+    new_password = request.form.get("password", None)
+    result = update_data(User, db, id, username=new_username, password=new_password)
+    return jsonify(result)
 
 @users_routes.route("/users/<int:id>", methods=["DELETE"])
 def delete_user(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify(user.serialize())
+    result = delete_data_by_id(User, id, db)
+    return jsonify(result)
